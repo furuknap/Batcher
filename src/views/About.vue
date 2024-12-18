560e95531a5a13b2ac80fea5b758dd01bbbc498e
Bjørn Furuknap <furuknap@users.noreply.github.com>
2024-12-18 13:10:46 -0500

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,7 +3,7 @@
     <div class="about">
         <h2>{{ $t('menu.about') }}</h2>
         <p>This is Batcher, an app to manage your batches of candied almonds and cookies.</p>
-        <p><a href="https://your-website.com" target="_blank">More Information</a></p>
+        <p><a href="https://almendras.co/" target="_blank">More Information (almendras.co/)</a></p>
     </div>
 </template>
 
